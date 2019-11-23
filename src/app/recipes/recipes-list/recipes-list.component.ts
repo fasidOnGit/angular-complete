1b790fd0b97a4e266cfe299ca002caf92005ff1f
Kader Fasid <fasidmpm@gmai.com>
2019-11-23 14:55:48 +0530

    Components

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [];
+  constructor() {
+    this.recipes.push(
+      new Recipe(
+        'whatever',
+        'description',
+        'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/master/pass/Basically-Gojuchang-Chicken-Recipe-Wide.jpg'
+      ),
+      new Recipe(
+        'whatever',
+        'description',
+        'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/master/pass/Basically-Gojuchang-Chicken-Recipe-Wide.jpg'
+      )
+    );
+  }
+
+  ngOnInit() {
+  }
+
+}
