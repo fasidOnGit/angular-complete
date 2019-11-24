ec82fe406790a6700f14716175723ff417966df0
Kader Fasid <fasidmpm@gmai.com>
2019-11-24 21:06:39 +0530

    Added custom validators

--- forms/src/app/customers/customer.component.ts
+++ forms/src/app/customers/customer.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Customer } from './customer';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+function ratingRange(c: AbstractControl): {[key: string]: boolean} | null {
+  if (c.value !== null && (isNaN(c.value) || c.value < 1 || c.value > 5)) {
+    return {range: true};
+  }
+  return null;
+}
 
 @Component({
   selector: 'app-customer',
@@ -26,6 +33,9 @@ export class CustomerComponent implements OnInit {
       firstName: ['', [Validators.required,  Validators.minLength(3)]],
       lastName: [{value: '', disabled: false}, [Validators.required, Validators.maxLength(50)]],
       email: ['', [Validators.required, Validators.email]],
+      phone: '',
+      notification: 'email',
+      rating: [null, ratingRange],
       sendCatalog: true
     });
   }
@@ -57,5 +67,15 @@ export class CustomerComponent implements OnInit {
       lastName: 'Hackness',
       sendCatalog: false
     });
+  }
+
+  setNotification(notifyVia: string) {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
   }
 }
